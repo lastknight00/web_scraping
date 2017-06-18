fd6c8df20fe34f24ac43e281e645a23b91a46274
taehan Lee <darkknight8608@gmail.com>
2017-06-18 18:57:31 +0900

    getAllLinks and scrapy library added

---
+++ src/ch03/internet.py
@@ -0,0 +1,74 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import datetime
+import random
+
+pages = set()
+random.seed(datetime.datetime.now())
+
+def getInternalLinks(bsObj, includeUrl):
+    internalLinks = []
+    for link in bsObj.findAll("a", href=re.compile("^(/|.*"+includeUrl+")")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in internalLinks:
+                internalLinks.append(link.attrs['href'])
+    return internalLinks
+
+def getExternalLinks(bsObj, excludeUrl):
+    externalLinks = []
+    for link in bsObj.findAll("a", href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
+        if link.attrs["href"] is not None:
+            if link.attrs["href"] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
+
+def splitAddress(address):
+    addressParts = address.replace("http://", "").split("/")
+    return addressParts
+
+def getRandomExternalLink(startingPage):
+    html = urlopen(startingPage)
+    bsObj = BeautifulSoup(html, "html.parser")
+    externalLinks = getExternalLinks(bsObj, splitAddress(startingPage)[0])
+    if len(externalLinks) == 0:
+        internalLinks = getInternalLinks(bsObj, startingPage)
+        return followExternalOnly(internalLinks[random.randint(0, len(internalLinks) - 1)])
+    else:
+        return externalLinks[random.randint(0, len(externalLinks) - 1)]
+
+def followExternalOnly(startSite):
+    externalLink = getRandomExternalLink(startSite)
+    print("Random external link is: " + externalLink)
+    followExternalOnly(externalLink)
+
+#followExternalOnly("http://oreilly.com")
+
+allExtLinks = set()
+allIntLinks = set()
+
+def getAllExternalLinks(siteUrl):
+    html = urlopen(siteUrl)
+    bsObj = BeautifulSoup(html, "html.parser")
+    internalLinks = getInternalLinks(bsObj, splitAddress(domain)[0])
+    externalLinks = getExternalLinks(bsObj, splitAddress(domain)[0])
+
+    for link in externalLinks:
+        if link not in allExtLinks:
+            allExtLinks.add(link)
+
+    for link in internalLinks:
+        if link == "/":
+            link = domain
+        elif link[0:2] == "//":
+            link = "http:" + link
+        elif link[0:1] == "/":
+            link = domain + link
+
+        if link not in allIntLinks:
+            print("About to get link: " + link)
+            allIntLinks.add(link)
+            getAllExternalLinks(link)
+
+domain = "http://oreilly.com"
+getAllExternalLinks(domain)
